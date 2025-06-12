edff4b7182012e5178f1578533dfb9ce72626617
DrMikeyS <mike@mikedavidsmith.com>
2025-06-12 16:55:29 +0100

    Adding demo page

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const config = {
+export default {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+		pages: 'build',
+		assets: 'build'
+		}),
+    paths: {
+      base: process.env.NODE_ENV === 'production' ? '/hazardwise' : '',
+    }
+  }
 };
-
-export default config;
