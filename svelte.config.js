c8e56167542a4c784653a8838bb396f7db8db137
DrMikeyS <mike@mikedavidsmith.com>
2025-06-12 17:18:04 +0100

    Attempted to build for ghpages

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,10 @@ export default {
 	kit: {
 		adapter: adapter({
 		pages: 'build',
-		assets: 'build'
+		assets: 'build',
+		fallback: 'index.html',
+		// don't fail when encountering dynamic routes
+		strict: false
 		}),
     paths: {
       base: process.env.NODE_ENV === 'production' ? '/hazardwise' : '',
