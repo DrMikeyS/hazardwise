5d23fc1c964a674d3dbf2d58b6361aa4113d2dff
DrMikeyS <mike@mikedavidsmith.com>
2025-06-17 08:17:35 +0100

    minor changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,13 @@
-//svelte.config.js
+// svelte.config.js
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 export default {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-		pages: 'docs',
-		assets: 'docs',
-		fallback: 'index.html',
-		// don't fail when encountering dynamic routes
-		strict: false
-		}),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/hazardwise' : '',
-    }
+  preprocess: vitePreprocess(),
+  kit: {
+    // use the static adapter with all the defaults
+    adapter: adapter(),
+    // remove any custom base path unless you really need it
+    // paths: { base: '' },
   }
 };
