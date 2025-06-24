64ec687c8ff114edb3b8c245d03310ccb6191c8d
DrMikeyS <mike@mikedavidsmith.com>
2025-06-24 17:01:05 +0100

    deploy 2025-06-24 17:01:05

--- _app/immutable/chunks/DBazd1J6.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_13y1a9d)==null?void 0:s.base)??"/hazardwise";var a;const t=((a=globalThis.__sveltekit_13y1a9d)==null?void 0:a.assets)??e;export{t as a,e as b};
